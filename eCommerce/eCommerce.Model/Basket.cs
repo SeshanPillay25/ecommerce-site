06e76891a92e30da0dd8a8f1c57939e9ffef1cb2
Seshan Pillay <30171059+SeshanPillay25@users.noreply.github.com>
2018-01-28 17:23:04 +0200

    Add files via upload

---
+++ eCommerce/eCommerce.Model/Basket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Model
+{
+    public class Basket
+    {
+        public int BasketId { get; set; }
+        public DateTime date { get; set; }
+
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        public void createNewBasket(System.Web.HttpContextBase httpContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
